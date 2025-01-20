3aa38bb690fbe9d52d78b839664e64cafab0a29c
nimbus <zerotonimbus@gmail.com>
2025-01-20 18:33:54 +0300

    initial commit

---
+++ src/app/account/page.jsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Account",
+};
+
+function Page() {
+  return <div>Page</div>;
+}
+
+export default Page;
---
+++ src/app/layout.jsx
@@ -0,0 +1,34 @@
+import Header from "@/components/Header";
+import "@/styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
+
+export const metadata = {
+  title: {
+    default: "Hutlet",
+    template: "%s - Hutlet",
+  },
+
+  description:
+    "Explore a variety of luxurious cabin hotels for your perfect getaway, located in the heart of Italian dolomites, surrounded by beautiful mountains and forests.",
+};
+
+function RootLayout({ children }) {
+  return (
+    <html lang={"en"}>
+      <body
+        className={
+          "bg-primary-950 antialiased relative text-primary-100 min-h-screen flex flex-col " +
+          josefin.className
+        }
+      >
+        <Header />
+        <div className={"flex-1 px-8 py-12"}>
+          <main className={"mx-auto"}>{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
